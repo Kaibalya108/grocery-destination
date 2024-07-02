9bdd13fcaeb6d6c1fa6575c6d3613a235831edae
Kaibalya108 <kaibalyaroutray21@gmail.com>
2024-07-02 21:41:37 +0530

    updated

--- htdocs/grocery_initial/admin/delete_product.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('connect.php');
-
-$get_id=$_GET['id'];
-
-mysqli_query($conn, "delete from tb_products where productID='$get_id'")or die(mysqli_error());
-header('location:product.php');
-?>
